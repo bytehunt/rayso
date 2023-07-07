cef89680b040f67b79c706d3571774b37d53de5c
pwnwriter <hey@pwnwriter.xyz>
2023-07-07 09:40:19 +0545

    feat(opts): options for ✨ padding,theme and many more

--- src/ui/args.rs
+++ src/ui/args.rs
@@ -1,13 +1,31 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = "None")]
+#[command(author, version, about, long_about = "✨Upload code to ray.so from terminal")]
 pub struct Cli {
     #[arg(short, long)]
-    /// filename
+    /// specify the filename to upload
     pub filename: String,
 
-    #[arg(short, long, default_value = "/usr/bin/brave")]
-    /// Browser to open the url
-    pub browser: String,
+    #[arg(short, long, default_value = "brave")]
+    /// specify the browser to open the URL
+    pub open: String,
+
+    #[arg(short, long, default_value = "candy")]
+    /// specify the theme of the image
+    /// Available options: Breeze, candy, Crimson, Falcon, Meadow, Midnight, Raindrop, Sunset
+    pub theme: String,
+
+    #[arg(short, long, default_value = "false")]
+    /// specify if the background should be enabled
+    pub background: bool,
+
+    #[arg(short, long, default_value = "32")]
+    /// specify the padding of the image
+    pub padding: i32,
+
+    #[arg(short, long, default_value = "false")]
+    /// specify if dark mode should be toggled
+    pub darkmode: bool,
 }
+
--- src/ui/rayso.rs
+++ src/ui/rayso.rs
@@ -1,20 +1,46 @@
-use std::fs;
+use open;
 use rbase64;
+use std::fs;
 use clap::Parser;
 use crate::ui::args::Cli;
-use open;
+
+pub fn generate_url(
+    theme: &str,
+    background: &bool,
+    padding: &i32,
+    darkmode: &bool,
+    base64_encoded: &str,
+    filename: &str,
+) -> String {
+    let ray_url = "https://ray.so/#code=";
+    format!(
+        "{}{}&darkMode={}&theme={}&title={}&background={}&padding={}",
+        ray_url, base64_encoded, darkmode, theme, filename, background, padding
+    )
+}
 
 pub fn ray() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
-    let ray_url = "https://ray.so/#code=";
     let filename = &cli.filename;
+    let browser = &cli.open;
+
     let file_contents = fs::read(filename)?;
     let base64_encoded = rbase64::encode(&file_contents);
-    let joined_url = format!("{}{}", ray_url, base64_encoded);
+
+    let joined_url = generate_url(
+        &cli.theme,
+        &cli.background,
+        &cli.padding,
+        &cli.darkmode,
+        &base64_encoded,
+        filename,
+    );
+
     println!("{}", joined_url);
-    let browser = &cli.browser;
     open::with(&joined_url, browser)?;
 
     Ok(())
 }
+
+
 
